44e1ea35317c2df6c2fd1787158b1c5d87e1be73
partymaker-py <partymaker-py@yandex.ru>
2020-11-30 09:04:58 -0500

    TELEGRAM + EMAIL

--- DMU/apps/main/views.py
+++ DMU/apps/main/views.py
@@ -3,12 +3,54 @@ from .models import New, Context, PostContact, PostCareer
 from django.db.models import Q
 from django.http import Http404
 from urllib.parse import unquote
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+
 import json
 import requests
+import smtplib
 
+class Additional():
 
-API_TOKEN = '1415059752:AAGzQtLrO-l7qLDPoCbbubVTX8gyXmTWau4'
-CHAT_ID = '-444340839'
+	def send_mail(dic):
+
+		smtp_host = "smtp.gmail.com"
+		smtp_port = "587"
+		smtp_login = "dmu.feedback@gmail.com"
+		smtp_password = "Str0ytr@nsg@z"
+
+		message = dic['Форма'] + '\n'
+
+		for key in dic:
+			if key != 'Форма':
+				if key == 'Сообщение':
+					message += '\n' + key + ': ' + '\n' + dic[key]
+				else:
+					message += key + ': ' + dic[key] + '\n'
+
+		msg = MIMEMultipart()
+		msg.attach(MIMEText(message, 'plain'))
+
+		server = smtplib.SMTP('smtp.gmail.com: 587')
+		server.starttls()
+		server.login(smtp_login, smtp_password)
+		server.sendmail(smtp_login, 'lil.kirill1488228@gmail.com', msg.as_string())
+		server.quit()
+
+	def telegram(dic):
+
+		API_TOKEN = '1415059752:AAGzQtLrO-l7qLDPoCbbubVTX8gyXmTWau4'
+		CHAT_ID = '-444340839'
+		
+		DATA = 'Форма ' + dic['Форма'] + '\n\n'
+		for key in dic:
+			if key != 'Форма':
+				DATA += key + ": " + dic[key] + '\n'
+
+		url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=html&text={}'.format(API_TOKEN, CHAT_ID, DATA)
+		r = requests.get(url)
+
 
 # Поиск + отображение главной страницы
 def index(request):
@@ -43,6 +85,7 @@ def contacts(request):
 		data = json.loads(request.body)
 
 		dic = {
+			'Форма' : 'Контакты',
 			'Имя': data.get('name', None),
 			'Телефон': data.get('phone', None),
 			'Email': data.get('email', None),
@@ -62,19 +105,9 @@ def contacts(request):
 					email = dic['Email'],
 					message = dic['Сообщение']
 				)
-
-			DATA = 'Форма: Контакты\n \n'
-			for key in dic:
-				DATA += key + ": " + dic[key] + '\n'
-
-			url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=html&text={}'.format(API_TOKEN, CHAT_ID, DATA)
-
-			r = requests.get(url)
-
-			if r:
-				print('TELEGRAM SUCCESS')
-			else:
-				print('TELEGRAM ERROR')
+			
+			Additional.telegram(dic)
+			Additional.send_mail(dic)
 
 			return render(request, 'main/contacts.html')
 		else:
@@ -89,6 +122,7 @@ def career(request):
 		data = json.loads(request.body)
 
 		dic = {
+			'FORM': 'CAREER',
 			'Имя': data.get('name', None),
 			'Отчество': data.get('patronymic', None),
 			'Фамилия': data.get('surname', None),
@@ -112,18 +146,8 @@ def career(request):
 				message = dic['Сообщение']
 			)
 
-			DATA = 'Форма: Карьера\n \n'
-			for key in dic:
-				DATA += key + ": " + dic[key] + '\n'
-
-			url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=html&text={}'.format(API_TOKEN, CHAT_ID, DATA)
-
-			r = requests.get(url)
-
-			if r:
-				print('TELEGRAM SUCCESS')
-			else:
-				print('TELEGRAM ERROR')
+			Additional.telegram(dic)
+			Additional.send_mail(dic)
 
 		else:
 			# Исход попытки внедрения зловредного кода в БД
