cf02812872e814044b700bce55e525e7be496378
Boris Ponomarenko <borisponomarenko@gmail.com>
2020-12-01 18:44:54 +0300

    Boris: search for company page

--- DMU/static/js/aboutCompany.js
+++ DMU/static/js/aboutCompany.js
@@ -1,3 +1,4 @@
+const searchForm = document.querySelector('.search-form');
 const firstLine = document.querySelector('.first-line');
 const secondLine = document.querySelector('.second-line');
 const controlPrev = document.querySelectorAll('.btnPrev');
@@ -87,5 +88,21 @@ function thanksgivingPresentation() {
     });
 };
 
+searchForm.addEventListener('submit', e => { 
+    e.preventDefault()
+    const searchWord = document.querySelector('.search-in-aboutCompany');
+    const regExp = /[<>{}]/;
+
+    if (!searchWord.value || searchWord.value.match(regExp)){
+        searchForm.style.border = '1px solid rgba(255, 0, 0, 1)';
+        setTimeout(() => {
+            searchForm.style.border = '';
+            searchWord.value = '';
+        }, 1000);
+        return;
+    } 
+    fetch(location.replace(`http://127.0.0.1:8000/?search=${searchWord}`)).then(res => console.log(res)).catch(rej => console.log(rej))
+});
+    
 
 thanksgivingPresentation();
