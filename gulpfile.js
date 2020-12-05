a81e6ae2d5780c85830ec4783742746e9676ff55
Boris Ponomarenko <borisponomarenko@gmail.com>
2020-12-05 22:54:22 +0300

    Boris: add a meta data and adoptate aboutCompany

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
  
 exports.default = () => (
-    gulp.src('/Users/boris/DMU/DMU/static/dist/**.css')
+    gulp.src('/Users/boris/DMU/DMU/static/styles/aboutCompanyStyle.css')
         .pipe(sourcemaps.init())
         .pipe(autoprefixer())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('new'))
 );
 exports.default = () => (
-    gulp.src('/Users/boris/DMU/DMU/static/styles/**.css')
+    gulp.src('/Users/boris/DMU/DMU/static/styles/aboutCompanyStyle.css')
         .pipe(autoprefixer({
             cascade: false
         }))
