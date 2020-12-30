5945af92fbc20e88f06ad4261872f9a00296d0a1
Boris Ponomarenko <borisponomarenko@gmail.com>
2020-12-30 20:48:36 +0300

    Boris: almost done webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,63 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+// TODO ипортировать css в соответствющие js файлы
+// TODO ипортировать статические картинки в корректные js файлы
+// TODO доделать опимизацию html файлов
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  config.minimizer = [
+    new OptimizeCssAssetsWebpackPlugin(),
+    new TerserWebpackPlugin()
+  ]
+  return config
+}
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: './DMU/static/js/main.js',
-        contacts: './DMU/static/js/contacts.js',
-        career: './DMU/static/js/career.js',
-        aboutCompany: './DMU/static/js/aboutCompany.js',
-        headerScrollAndPopupModule: './DMU/static/js/Modules/headerScrollAndPopupModule.js',
-        movingCardsModule: './DMU/static/js/Modules/movingCardsModule.js',
-        searchFromPageModule: './DMU/static/js/Modules/searchFromPageModule.js',
-        sliderUltraModule: './DMU/static/js/Modules/sliderUltraModule.js'
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'DMU/static/FonrontBuild')
-    },
-    plugins: [
-        new CleanWebpackPlugin(), 
-    ],
-    module: {
-        rules: [
-            {
-                // конфиг для стилей
-                test:/\.css$/,
-                use: ["style-loader", "css-loader", "postcss-loader"]
-            },
-            {
-                // конфиг для шрифта (шрифты должны быть в отдельном файле)
-                test: /\.(ttf|wof)$/,
-                use: ['file-loader']
-            }
-        ]
-    }
-    
+  context: 'C:/Users/boris/DMU/DMU/static', // TODO Изменить путь (Абсолютный)
+  mode: 'development',
+  entry: {
+    mainPage: './js/main.js',
+    careerPage: './js/career.js',
+    contactsPage: './js/contacts.js',
+    aboutCompanyPage: './js/aboutCompany.js',
+    movingCardsModule: './js/Modules/movingCardsModule.js',
+    headerScrollAndPopupModule: './js/Modules/headerScrollAndPopupModule.js',
+    searchFromPageModule: './js/Modules/searchFromPageModule.js',
+    sliderUltraModule: './js/Modules/sliderUltraModule.js'
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, './DMU/static/dist')
+  },
+  optimization: optimization(),
+  plugins: [
+    new HTMLWebpackPlugin({
+      // template: './DMU/templates/index.html',
+      minify: {
+        collapseWhitespace: true // оптимизирует html
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+    ]
+  },
 }
