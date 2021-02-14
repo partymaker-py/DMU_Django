9c36991649426f7fb9cb41bf2e80522541f67a8c
Boris Ponomarenko <borisponomarenko@gmail.com>
2021-02-14 16:02:28 +0300

    Boris: debug counters

--- DMU/static/js/main.js
+++ DMU/static/js/main.js
@@ -9,48 +9,27 @@ const titleOfPage = document.querySelector('.title-of-page');
 const knowMoreLink = document.querySelector('.know-more-link');
 
 //  ФУНКЦИЯ СЧЁТЧИКИ
-const getCounters = (begin1, end1, begin2, end2, begin3, end3) => {
-
-    let current1 = begin1;
-    let current2 = begin2;
-    let current3 = begin3;
-
-    let timerId1 = setInterval(function () {
-        if (current1 == end1) {
-            clearInterval(timerId1);
-        }
-
-        projectsCounter.textContent = `${current1++}`;
-    }, 200);
-
-    let timerId2 = setInterval(function () {
-        if (current2 === end2) {
-            clearInterval(timerId2);
-        }
-
-        yearsCounter.textContent = `${current2++}`;
+const getCounters = (start, end, inner) => {
+    let startWith = start;
+    let timer = setInterval(() => {
+        if (startWith === end) clearInterval(timer);
+        inner.textContent = startWith++;
+        
     }, 100);
-
-    let timerId3 = setInterval(function () {
-        if (current3 === end3) {
-            clearInterval(timerId3);
-        }
-        customersCounter.textContent = `${current3++}`;
-    }, 100);
-};
+}
 
 // вызов счётчиков
-let counterOfCounters;
-window.window.addEventListener('scroll', () => {
+let controller = false;
+document.addEventListener('scroll', () => {
     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-        
-        if (counterOfCounters === undefined) {
-            getCounters(0, 279, 0, 1777, 0, 20);
-            counterOfCounters = ':(-это пасхалка хехе';
-            console.log(counterOfCounters);
-        }
+        if (controller) return;
+        controller = true;
+        getCounters(1, 5, projectsCounter); // кол-во проектов
+        getCounters(1480, 1500, customersCounter); // кол-во сотрудников
+        getCounters(0, 3, yearsCounter); // кол-во лет
     }
 });
+
 // Функция движения заголовка главной страници
 const pageTitle = () => {
     let titleHightControl = 0;
