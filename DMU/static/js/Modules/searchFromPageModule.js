572ac6040fa649cde67c5b73b2401dc37a5bc8bb
Boris Ponomarenko <borisponomarenko@gmail.com>
2021-02-16 22:07:09 +0300

    Boris: debbug adoptation

--- DMU/static/js/Modules/searchFromPageModule.js
+++ DMU/static/js/Modules/searchFromPageModule.js
@@ -1,17 +1,19 @@
-const searchForm = document.querySelector('.search-form');
-searchForm.addEventListener('submit', e => { 
-    e.preventDefault();
-    const searchWord = document.querySelector('.get-search-from-page');
-    const regExp = /[<>{}]/;
+document.addEventListener('load', () => {
+    const searchForm = document.querySelector('.search-form');
+    searchForm.addEventListener('submit', e => { 
+        e.preventDefault();
+        const searchWord = document.querySelector('.get-search-from-page');
+        const regExp = /[<>{}]/;
 
-    if (!searchWord.value || searchWord.value.match(regExp)){
-        searchForm.style.border = '1px solid rgba(255, 0, 0, 1)';
-        setTimeout(() => {
-            searchForm.style.border = '';
-            searchWord.value = '';
-        }, 1000);
-        return;
-    } 
-    fetch(location.replace(`http://127.0.0.1:8000/?search=${searchWord.value}`)).then(res => console.log(res)).catch(rej => console.log(rej));
-    searchWord.value = '';
-});
+        if (!searchWord.value || searchWord.value.match(regExp)){
+            searchForm.style.border = '1px solid rgba(255, 0, 0, 1)';
+            setTimeout(() => {
+                searchForm.style.border = '';
+                searchWord.value = '';
+            }, 1000);
+            return;
+        } 
+        fetch(location.replace(`http://127.0.0.1:8000/?search=${searchWord.value}`)).then(res => console.log(res)).catch(rej => console.log(rej));
+        searchWord.value = '';
+    });
+})
